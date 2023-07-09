a8e85a8b313e2c4866f498c6bd735afbb2a2be37
dasol729 <97594634+dasol729@users.noreply.github.com>
2023-07-09 21:31:08 +0900

    0709 헤더 레이아웃 보완 (완성)
    0709 헤더 보완

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,28 @@
 //import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-//import { Col, Row } from 'antd';
+import logo from '../contents/grid/Logo_플래그_Small_수정.svg';
 
 const HeaderCover = styled.div`
-  position: relative;
-  height: 80px;
+  height: 114px;
   margin: 0 auto;
   display: flex;
-  border: 2px solid #000000;
-  padding: 5% auto 5%;
+  padding: 5% auto auto;
+  white-space: nowrap;
 `;
-const HeaderLogo = styled.div`
-  border: 2px solid #000011;
-  margin: 20px;
-`;
-const HeaderMenuCover = styled.div`
-  position: relative;
-  width: 40%;
-  border: 2px solid #000011;
-  margin: 20px;
+const HeaderLogo = styled.span`
+  margin: auto 10% 20px 8%;
 `;
 const HeaderMenu = styled.span`
-  position: absolute;
-  right: 0;
-  border: 2px solid #000011;
-  margin: 5%;
+  font-color: #272727;
+  font-size: 22px;
+  font-style: normal;
+  margin: auto 50px 20px 0px;
 `;
-const HeaderLogin = styled.div`
-  position: absolute;
-  right: 0;
-  border: 2px solid #000011;
-  margin: 20px;
+const HeaderLogin = styled.span`
+  font-color: #272727;
+  font-size: 22px;
+  font-style: normal;
+  margin: auto 10% 20px auto;
 `;
 const HeaderLine = styled.hr`
   border: 0.5px solid rgba(142, 111, 255, 0.8);
@@ -41,16 +33,11 @@ function Header() {
     <div>
       <HeaderCover>
         <HeaderLogo>
-          <img
-            src="./../contents/grid/LOGO_플래그_Small_수정.svg"
-            alt="img_not_found..."
-          />
+          <img src={logo} alt="img_not_found..." />
         </HeaderLogo>
-        <HeaderMenuCover>
-          <HeaderMenu>서비스 안내</HeaderMenu>
-          <HeaderMenu>플래그</HeaderMenu>
-          <HeaderMenu>플렉스</HeaderMenu>
-        </HeaderMenuCover>
+        <HeaderMenu>서비스 안내</HeaderMenu>
+        <HeaderMenu>플래그</HeaderMenu>
+        <HeaderMenu>플렉스</HeaderMenu>
         <HeaderLogin>로그인</HeaderLogin>
       </HeaderCover>
       <HeaderLine />
