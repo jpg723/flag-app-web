f2a7b3712a81d6c5aee9130e6a061e187439528e
dasol729 <97594634+dasol729@users.noreply.github.com>
2023-07-09 15:16:46 +0900

    0709 헤더보완, contents저장
    0709 헤더보완, contents저장

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,24 +3,34 @@ import styled from 'styled-components';
 //import { Col, Row } from 'antd';
 
 const HeaderCover = styled.div`
-  height: 100px;
+  position: relative;
+  height: 80px;
   margin: 0 auto;
   display: flex;
-  flex-direction: row;
-  flex-grow: 1;
   border: 2px solid #000000;
+  padding: 5% auto 5%;
 `;
 const HeaderLogo = styled.div`
   border: 2px solid #000011;
-  margin: 25px;
+  margin: 20px;
 `;
-const HeaderMenu = styled.div`
+const HeaderMenuCover = styled.div`
+  position: relative;
+  width: 40%;
   border: 2px solid #000011;
-  margin: 25px;
+  margin: 20px;
+`;
+const HeaderMenu = styled.span`
+  position: absolute;
+  right: 0;
+  border: 2px solid #000011;
+  margin: 5%;
 `;
 const HeaderLogin = styled.div`
+  position: absolute;
+  right: 0;
   border: 2px solid #000011;
-  margin: 25px;
+  margin: 20px;
 `;
 const HeaderLine = styled.hr`
   border: 0.5px solid rgba(142, 111, 255, 0.8);
@@ -31,14 +41,16 @@ function Header() {
     <div>
       <HeaderCover>
         <HeaderLogo>
-          <img src="../contents/LOGO2.png" />
-          <span>fleg</span>
+          <img
+            src="./../contents/grid/LOGO_플래그_Small_수정.svg"
+            alt="img_not_found..."
+          />
         </HeaderLogo>
-        <HeaderMenu>
-          <span>서비스 안내</span>
-          <span>플래그</span>
-          <span>플렉스</span>
-        </HeaderMenu>
+        <HeaderMenuCover>
+          <HeaderMenu>서비스 안내</HeaderMenu>
+          <HeaderMenu>플래그</HeaderMenu>
+          <HeaderMenu>플렉스</HeaderMenu>
+        </HeaderMenuCover>
         <HeaderLogin>로그인</HeaderLogin>
       </HeaderCover>
       <HeaderLine />
