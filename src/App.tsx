8cd61b178f546f806caa9b89cb5d933c67e5a9de
jpg723 <86431761+jpg723@users.noreply.github.com>
2023-08-14 20:02:50 +0900

    [UPDATE] 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
 import Header from './components/Header';
+import {Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import ComfirmedPromise from './pages/ComfirmedPromise';
+import PromiseView from './pages/PromiseView';
+import FlagMeeting from './pages/FlagMeeting';
+
+
 
 function App() {
-  return <Header />;
+  return (
+    <div>
+        <Header></Header>
+        <Router>
+          <Routes>
+            <Route path="/promise-view" element={<PromiseView />} />
+            <Route path="/flag-meeting" element={<FlagMeeting />} />
+            <Route path="/comfirmed-promise" element={<ComfirmedPromise />} />
+          </Routes>
+        </Router>
+    </div>
+  );
 }
+
 export default App;
