a64e8875e52fd099ca2e5ee087a2abc0c6708fae
Lee-Sunho <sunho9889@naver.com>
2023-08-14 18:09:37 +0900

    develop merge용 App, index 코드 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,7 @@
 import React from 'react';
-import MakeFlag from './pages/MakeFlag';
-import MakeFlagFinish from './pages/MakeFlagFinish';
-import Main from './pages/Main';
 import Header from './components/Header';
-import ScrollToTop from './components/ScrollToTop';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
-import { GlobalStyle } from './GlobalStyle';
-import ServiceInfo from './pages/ServiceInfo';
 
 function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Header />
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route
-            path="/serviceInfo"
-            element={<ServiceInfo />}
-          />
-          <Route path="/makeFlag" element={<MakeFlag />} />
-          <Route
-            path="/makeFlagFinish"
-            element={<MakeFlagFinish />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+  return <Header />;
 }
 export default App;
